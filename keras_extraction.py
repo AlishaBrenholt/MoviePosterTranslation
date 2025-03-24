817ab6cc2a1100743a875d9d09fe08dab8bdad52
kiyj12 <kiyj12@gmail.com>
2025-03-23 21:00:13 -0400

    blur the text part

--- keras_extraction.py
+++ keras_extraction.py
@@ -1,13 +1,16 @@
 import keras_ocr
 import matplotlib.pyplot as plt
 import cv2
+import numpy as np
 
-def keras_extractor(data_path, poster_name):
+def keras_extractor(data_path, poster_name, blur_strength=(190,190)):
     # To check if the image is shown correctly
     # original_image = cv2.imread(data_path+poster_name)
     # original_image = original_image[:, :, ::-1]
     # plt.imshow(original_image)
     # plt.show()
+    # Ensure blur kernel size is odd
+    blur_strength = tuple((s if s % 2 == 1 else s + 1) for s in blur_strength)
 
     # Initialize pipeline
     pipeline = keras_ocr.pipeline.Pipeline()
@@ -16,7 +19,35 @@ def keras_extractor(data_path, poster_name):
     # result is a list of (word, box) tuples
     prediction_groups = pipeline.recognize([read_image])
     # Plot the predictions
-    keras_ocr.tools.drawAnnotations(image=read_image, predictions=prediction_groups[0])
+    # keras_ocr.tools.drawAnnotations(image=read_image, predictions=prediction_groups[0])
 
-    plt.show()
+    # Blurring the bounding box
+    image = cv2.imread(data_path+poster_name)
+    for text, box in prediction_groups[0]:
+        box = np.array(box).astype(int)
+        x_min, y_min = np.min(box[:, 0]), np.min(box[:, 1])
+        x_max, y_max = np.max(box[:, 0]), np.max(box[:, 1])
+        # Blur each character
+        text_width = x_max - x_min
+        text_height = y_max - y_min
+        char_cnt = len(text)
+
+        if char_cnt > 0:
+            char_width = text_width / char_cnt
+
+            for i in range(char_cnt):
+                char_x_min = int(x_min + i * char_width)
+                char_x_max = int(x_min + (i + 1) * char_width)
+
+                if char_x_max > x_max:
+                    char_x_max = x_max
+                background_color = np.median(image[y_min:y_max, max(0, x_min - 5):min(image.shape[1], x_max + 5)],
+                                             axis=(0, 1))
+
+                image[y_min:y_max, char_x_min:char_x_max] = background_color
+
+
+    result_path = './results/blurred/'+ data_path[14:-4]+poster_name
+
+    cv2.imwrite(result_path, image)
     return prediction_groups
--- main.py
+++ main.py
@@ -22,80 +22,82 @@ except:
     tesseract_path = "/usr/local/bin/tesseract"
 
 # TODO : Make it iterative for later
-data_path = "./data/images/interstellar/en/"
+data_path = "./data/images/apocalypsenow/en/"
 poster_name = "en_1.jpg"
 '''keras extraction experiment lab'''
 data = ke.keras_extractor(data_path, poster_name)
 print(f"keras_data: {data}")
+
+
 '''lab done'''
 
-def get_text_from_poster(tesseract_path):
-    """
-    Extracts text from posters using tesseract. It also saves the images in the results/blurred directory.
-    Structure of data_dict:
-    movie {
-        poster {
-                {
-                    'level': [], 'page_num' : [], 'block_num' : [], 'line_num' : [], 'word_num' : [],
-                    'left' : [], 'top' : [], 'width' : [], 'height' : [], 'conf' : [], 'text' : []
-                }
-            }
-        }
-
-    :param data_path:
-    :param tesseract_path:
-    :return:
-    """
-    data_dict = {}
-    for movie_folder in os.listdir(movie_path):
-        data_dict[movie_folder] = {}
-        movie_folder_path = movie_path + movie_folder + "/"
-        eng_movie_folder_path = movie_folder_path + "en/"
-        kor_movie_folder_path = movie_folder_path + "ko/"
-        for poster in os.listdir(eng_movie_folder_path):
-            # ensure it ends with .jpg, ko has some .txt files
-            if poster.endswith(".jpg"):
-                data_dict[movie_folder][poster] = vision_model.tesseract_extractor(eng_movie_folder_path, poster, tesseract_path)
-    return data_dict
-
-def get_translations(data):
-    llm = LLMController()
-    translation_dict = {}
-    counter = 0
-    for movie, images in data.items():
-        print(f"Translating Movies: {counter}/{len(data)} - {movie}")
-        counter +=1
-        translation_dict[movie] = {}
-        for image, results in images.items():
-            translated_text = []
-            for text in results['text']:
-                if text == '':
-                    translated_text.append('')
-                else:
-                    translated_text.append(llm.translate_good(text))
-            translation_dict[movie][image] = translated_text
-            return translation_dict # TODO: Delete this line, it's just for testing and making it quicker
-    return translation_dict
-
-# create tesseract instance
-# conda install pytesseract
-# You can find the path using 'which tesseract' command in terminal in macOS
-load_dotenv()
-tesseract_path = get_tesseract_path()
-
-movie_path = "data/images/0GOODDATA/"
-data = get_text_from_poster(tesseract_path)
-translated_text = get_translations(data)
-
-# build the english, translated_text pair
-BLURRED_PATH = "results/blurred/"
-for movie, images in translated_text.items():
-    for image, text in images.items():
-        image_path = BLURRED_PATH + movie + "/" + image
-        # For sam's code? TODO still
-        # english_text = data[movie][image]['text']
-        # data[movie][image]['text'] = [english_text, text]
-
+# def get_text_from_poster(tesseract_path):
+#     """
+#     Extracts text from posters using tesseract. It also saves the images in the results/blurred directory.
+#     Structure of data_dict:
+#     movie {
+#         poster {
+#                 {
+#                     'level': [], 'page_num' : [], 'block_num' : [], 'line_num' : [], 'word_num' : [],
+#                     'left' : [], 'top' : [], 'width' : [], 'height' : [], 'conf' : [], 'text' : []
+#                 }
+#             }
+#         }
+#
+#     :param data_path:
+#     :param tesseract_path:
+#     :return:
+#     """
+#     data_dict = {}
+#     for movie_folder in os.listdir(movie_path):
+#         data_dict[movie_folder] = {}
+#         movie_folder_path = movie_path + movie_folder + "/"
+#         eng_movie_folder_path = movie_folder_path + "en/"
+#         kor_movie_folder_path = movie_folder_path + "ko/"
+#         for poster in os.listdir(eng_movie_folder_path):
+#             # ensure it ends with .jpg, ko has some .txt files
+#             if poster.endswith(".jpg"):
+#                 data_dict[movie_folder][poster] = vision_model.tesseract_extractor(eng_movie_folder_path, poster, tesseract_path)
+#     return data_dict
+#
+# def get_translations(data):
+#     llm = LLMController()
+#     translation_dict = {}
+#     counter = 0
+#     for movie, images in data.items():
+#         print(f"Translating Movies: {counter}/{len(data)} - {movie}")
+#         counter +=1
+#         translation_dict[movie] = {}
+#         for image, results in images.items():
+#             translated_text = []
+#             for text in results['text']:
+#                 if text == '':
+#                     translated_text.append('')
+#                 else:
+#                     translated_text.append(llm.translate_good(text))
+#             translation_dict[movie][image] = translated_text
+#             return translation_dict # TODO: Delete this line, it's just for testing and making it quicker
+#     return translation_dict
+#
+# # create tesseract instance
+# # conda install pytesseract
+# # You can find the path using 'which tesseract' command in terminal in macOS
+# load_dotenv()
+# tesseract_path = get_tesseract_path()
+#
+# movie_path = "data/images/0GOODDATA/"
+# data = get_text_from_poster(tesseract_path)
+# translated_text = get_translations(data)
+#
+# # build the english, translated_text pair
+# BLURRED_PATH = "results/blurred/"
+# for movie, images in translated_text.items():
+#     for image, text in images.items():
+#         image_path = BLURRED_PATH + movie + "/" + image
+#         # For sam's code? TODO still
+#         # english_text = data[movie][image]['text']
+#         # data[movie][image]['text'] = [english_text, text]
+#
 
 
 # pass filepath to edited poster and the data for it
