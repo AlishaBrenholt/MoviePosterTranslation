d4a270fb75cc48c46501910d517334c1ff9de73f
kiyj12 <kiyj12@gmail.com>
2025-03-23 20:12:29 -0400

    keras_extraction, main modified

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ except:
 
 # Load image
 # TODO : Make it iterative for later
-data_path = "./data/images/apocalypsenow/en/"
+data_path = "./data/images/interstellar/en/"
 poster_name = "en_1.jpg"
 # Extract texts and save text-extracted poster in results/blurred/
 '''
@@ -29,30 +29,30 @@ Structure of data
 
 data['text'] has the text contents that can go into LLM
 '''
-data = vision_model.tesseract_extractor(data_path, poster_name, tesseract_path) # This has text information including text contents and their location
+# data = vision_model.tesseract_extractor(data_path, poster_name, tesseract_path) # This has text information including text contents and their location
 
 '''keras extraction experiment lab'''
-ke_data = ke.keras_extractor(data_path, poster_name)
-print(f"keras_data: {ke_data}")
+data = ke.keras_extractor(data_path, poster_name)
+print(f"keras_data: {data}")
 '''lab done'''
 
 # Input data['text'] to LLM part
-llm = LLMController()
-translated_text = []
-for text in data['text']:
-    if text == '':
-        translated_text.append('')
-    else:
-        translated_text.append(llm.translate_good(text))
-# translated_text : translated version of data['text']
-# ex. llm_output = ['', '', '', '', '죽은', '시인들']
-# Put the output into data
-print(translated_text)
-eng_data_text = data['text']
-data['text'] = [eng_data_text, translated_text]
-# Input data into AnyText now.
-
-# pass filepath to edited poster and the data for it
-# stores in results/final_image after running
-final_poster = poster_generator.generate_poster("en_1--oem 3 --psm 11.jpg",
-                                                data)
+# llm = LLMController()
+# translated_text = []
+# for text in data['text']:
+#     if text == '':
+#         translated_text.append('')
+#     else:
+#         translated_text.append(llm.translate_good(text))
+# # translated_text : translated version of data['text']
+# # ex. llm_output = ['', '', '', '', '죽은', '시인들']
+# # Put the output into data
+# print(translated_text)
+# eng_data_text = data['text']
+# data['text'] = [eng_data_text, translated_text]
+# # Input data into AnyText now.
+#
+# # pass filepath to edited poster and the data for it
+# # stores in results/final_image after running
+# final_poster = poster_generator.generate_poster("en_1--oem 3 --psm 11.jpg",
+#                                                 data)
