a16babee53a65d4658c0d98f4ed1a31f79b8c9c3
samkissel <samkissl00@gmail.com>
2025-03-28 16:10:36 -0400

    fontsize calculated dynamically :-)

--- poster_generator.py
+++ poster_generator.py
@@ -7,9 +7,10 @@ def generate_poster(img_path, ko_text, text_data):
         korean_characters = item[0]
         positions = item[1]
         left = positions[0]
-        right = positions[1]
-    #   fontsize = (right[0]-left[0])//len(ko_text[0])
-        fontsize = 30
+        right = positions[2]
+        fontsize = (right[0]-left[0]) // len(korean_characters)
+        if fontsize == 0:
+            fontsize = 20
         font = ImageFont.truetype('./fonts/malgun-gothic.ttf', fontsize)
         text_color = (0, 0, 0)
         draw.text(left, korean_characters, text_color, font=font,
